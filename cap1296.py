4df9328b54bcbd0dcaa865ba6b00040cd92d0a58
wese3112 <wese3112@googlemail.com>
2018-04-26 11:44:28 +0200

    initial upload of sources

---
+++ cap1296.py
@@ -0,0 +1,38 @@
+# important registers
+MAIN_CONTROL = 0x00
+SENSOR_INPUT_STATUS = 0x03
+SENSOR_INPUT_ENABLE = 0x21
+INTERRUPT_ENABLE = 0x27
+SIGNAL_GUARD_ENABLE = 0x29
+MULTIPLE_TOUCH_CONFIG = 0x2A
+
+def _keys_to_byte(keys, default=b'\x00'):
+    return bytes([sum(map(lambda b: 1<<b, keys))]) if keys else default
+
+
+class CAP1296:
+    def __init__(self, i2c_handler):
+        self.i2c = i2c_handler
+        self._addr = 0x28
+    
+        self.write = lambda r, b: self.i2c.writeto_mem(self._addr, r, b)
+        self.read = lambda r, n: self.i2c.readfrom_mem(self._addr, r, n)
+
+    @property
+    def address(self):
+        return self._addr
+
+    def enable_interrupt(self, keys):
+        self.write(INTERRUPT_ENABLE, _keys_to_byte(keys, default=b'\x3f'))
+
+    def enable_keys(self, keys):
+        self.write(SENSOR_INPUT_ENABLE, _keys_to_byte(keys, default=b'\x3f'))   
+
+    def enable_signal_guard(self, keys):
+        assert 2 not in keys, 'cannot set key 2 (CS3), it is the SG channel'
+        self.write(SIGNAL_GUARD_ENABLE, _keys_to_byte(keys))
+
+    def read_keys(self):
+        status = self.read(SENSOR_INPUT_STATUS, 1)
+        self.write(MAIN_CONTROL, b'\x00')  # enables next touch reading
+        return int.from_bytes(status, 'little')
---
+++ cap1296_tests.py
@@ -0,0 +1,24 @@
+import unittest
+import cap1296
+
+
+class TestCAP1296Driver(unittest.TestCase):
+
+    def test_keys_to_byte(self):
+        input_result = (
+            ([], b'\x00'),
+            ([0], b'\x01'),
+            ([4], b'\x10'),
+            ([1,2], b'\x06'),
+            ([2,5], b'\x24'),
+            ([0,1,2,3,4,5], b'\x3f'),
+        )
+
+        for keys, output in input_result:
+            self.assertEqual(cap1296._keys_to_byte(keys), output)
+        
+        for default in [bytes([ii]) for ii in (13, 52, 1, 32)]:
+            self.assertEqual(cap1296._keys_to_byte([], default=default), default)
+
+if __name__ == '__main__':
+    unittest.main()
