8cf5a2d93c30d84c791850c382a1b06b5687d1fb
wese3112 <wese3112@googlemail.com>
2018-04-30 18:43:57 +0200

    - cap1296 I2C address as const - added _byte_to_keys function - added type hints

--- cap1296.py
+++ cap1296.py
@@ -4,6 +4,8 @@ except ImportError:
     def const(var):
         return var
 
+CAP1296_I2C_ADDRESS = const(0x28)
+
 # important registers
 MAIN_CONTROL = const(0x00)
 SENSOR_INPUT_STATUS = const(0x03)
@@ -13,14 +15,24 @@ SIGNAL_GUARD_ENABLE = const(0x29)
 MULTIPLE_TOUCH_CONFIG = const(0x2A)
 
 
-def _keys_to_byte(keys, default=b'\x00'):
+def _keys_to_byte(keys: list, default=b'\x00') -> bytes:
+    """Return a byte in which the bits X are 1 for each X in the list keys."""
     return bytes([sum(map(lambda b: 1 << b, keys))]) if keys else default
+
+def _byte_to_keys(keys_as_byte: bytes, num_keys=6) -> list:
+    """Return a list of key (bit) numbers for each 1 in keys_as_byte."""
+    keys_as_int = int.from_bytes(keys_as_byte, 'little')
+    return [
+        key
+        for key in range(num_keys)
+        if keys_as_int & (1 << key)
+    ]
 
 
 class CAP1296:
     def __init__(self, i2c_handler):
         self.i2c = i2c_handler
-        self._addr = 0x28
+        self._addr = CAP1296_I2C_ADDRESS
     
         self.write = lambda r, b: self.i2c.writeto_mem(self._addr, r, b)
         self.read = lambda r, n: self.i2c.readfrom_mem(self._addr, r, n)
@@ -29,13 +41,13 @@ class CAP1296:
     def address(self):
         return self._addr
 
-    def enable_interrupt(self, keys):
+    def enable_interrupt(self, keys: list):
         self.write(INTERRUPT_ENABLE, _keys_to_byte(keys, default=b'\x3f'))
 
-    def enable_keys(self, keys):
+    def enable_keys(self, keys: list):
         self.write(SENSOR_INPUT_ENABLE, _keys_to_byte(keys, default=b'\x3f'))   
 
-    def enable_signal_guard(self, keys):
+    def enable_signal_guard(self, keys: list):
         assert 2 not in keys, 'cannot set key 2 (CS3), it is the SG channel'
         self.write(SIGNAL_GUARD_ENABLE, _keys_to_byte(keys))
 
--- cap1296_tests.py
+++ cap1296_tests.py
@@ -3,9 +3,8 @@ import cap1296
 
 
 class TestCAP1296Driver(unittest.TestCase):
-
-    def test_keys_to_byte(self):
-        input_result = (
+    
+    test_data = (
             ([], b'\x00'),
             ([0], b'\x01'),
             ([4], b'\x10'),
@@ -14,12 +13,24 @@ class TestCAP1296Driver(unittest.TestCase):
             ([0, 1, 2, 3, 4, 5], b'\x3f'),
         )
 
-        for keys, output in input_result:
+    def test_keys_to_byte(self):
+        for keys, output in self.test_data:
             self.assertEqual(cap1296._keys_to_byte(keys), output)
         
+        # test the default keyword argument: giving an empty list should return
+        # the default value
         for default in [bytes([ii]) for ii in (13, 52, 1, 32)]:
             self.assertEqual(cap1296._keys_to_byte([], default=default), default)
+    
+    def test_byte_to_keys(self):
+        for output, keys_byte in self.test_data:
+            self.assertEqual(cap1296._byte_to_keys(keys_byte), output)
 
+        # test the num_keys keyword argument: hightest key num should be
+        # num_keys - 1 (counting from 0)
+        for num_keys in [5, 4, 3]:
+            expected = list(range(num_keys))
+            self.assertEqual(cap1296._byte_to_keys(b'\xff', num_keys=num_keys), expected)
 
 if __name__ == '__main__':
     unittest.main()
